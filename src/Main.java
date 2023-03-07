73f1e122581f06c844cfd77b5abbecf089f6c90c
anna <anna@sayf.su>
2023-03-07 19:38:04 +0300

    реализован Predicate

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /**Напишите реализации функционального интерфейса Predicate,
+         которые проверяют, является ли число положительным. Если число положительное, то предикат должен возвращать true,
+         в противном случае — false.
+         Реализуйте Predicate в двух вариантах: через анонимный класс, через лямбду.
+         */
+        List<Integer> integerList = new ArrayList<>();
+        integerList.add(1);
+        integerList.add(2);
+        integerList.add(-3);
+        integerList.add(-5);
+
+        for (Integer element : integerList) {
+                if(new Predicate<Integer>() {
+                    @Override
+                    public boolean test(Integer element) {
+                        return element > 0;
+                    }
+                }.test(element)) {
+                    System.out.println(element);
+                }
+        }
+
+        Predicate<Integer> predicate = el -> el > 0;
+        for (Integer element : integerList) {
+            if(predicate.test(element)) {
+                System.out.println(element);
+            }
+        }
+
     }
 }
